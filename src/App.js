bfd6bb15242e19ec2445f1ecd441790510f46faf
ozgedogru <ozgenurkarademir@gmail.com>
2023-10-31 19:28:49 +0300

    Form created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import "./App.css";
-import MemberForm from "./components/MemberForm";
+import Form from "./components/Form";
 
-function App() {
-  return <MemberForm />;
+function App(props) {
+  const { memberList, addMember } = props;
+
+  return (
+    <>
+      <h1>Uye Formu</h1>
+      <hr />
+      <Form />
+      <div addMember={addMember} memberList={memberList}>
+        <ul>
+          {memberList?.map((m) => {
+            return <li>{m.name}</li>;
+          })}
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,70 @@
+import { useEffect, useState } from "react";
+
+function Form(props) {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    rol: "",
+  });
+
+  const [memberList, setMemberList] = useState([
+    { name: "ozge", email: "ozgeedogruu@gmail.com" },
+  ]);
+
+  function submitHandler(e) {
+    console.log("Uye eklendi");
+    e.preventDefault();
+
+    addMember(formData);
+  }
+
+  function addMember(member) {
+    setMemberList([...memberList, member]);
+  }
+  useEffect(() => {
+    console.log(memberList);
+  }, [memberList]);
+
+  useEffect(() => {
+    console.log(formData);
+  }, [formData]);
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div>
+        <label htmlFor="name">Ad/Soyad : </label>
+        <input
+          id="name"
+          type="text"
+          onChange={(e) => {
+            setFormData({ ...formData, name: e.target.value });
+          }}
+        />
+      </div>
+      <div>
+        <label htmlFor="user-mail">E-mail : </label>
+        <input
+          id="user-mail"
+          type="email"
+          onChange={(e) => {
+            setFormData({ ...formData, email: e.target.value });
+          }}
+        />
+      </div>
+      <div>
+        <label htmlFor="rol">Rol : </label>
+        <input
+          id="rol"
+          type="text"
+          onChange={(e) => {
+            setFormData({ ...formData, rol: e.target.value });
+          }}
+        />
+      </div>
+
+      <button type="submit">Ekle!</button>
+    </form>
+  );
+}
+
+export default Form;
